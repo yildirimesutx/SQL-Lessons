741a6560b4b13bf92f9280c7c7ddafee74b23d04
mesut <yildirimesutcw@gmail.com>
2021-12-09 00:34:00 +0300

    first commit

---
+++ session-1.sql
@@ -0,0 +1,20 @@
+--SELECT
+SELECT name FROM tracks ;
+
+/* track tablsoundaki besteci şarkı isimleri */
+
+SELECT Composer, name FROM tracks;
+
+/* track tablsoundaki tüm bilgiler */
+
+SELECT *FROM tracks;
+
+--DISTINCT, tekrar eden özellikleri atlar
+
+SELECT DISTINCT Composer FROM tracks;
+
+SELECT DISTINCT AlbumId, MediaTypeId FROM tracks;
+
+SELECT name FROM tracks WHERE Composer="Jimi Hendrix" ;
+
+SELECT *FROM invoices WHERE total>10 ;
